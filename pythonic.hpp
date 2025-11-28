be55a6e5b54931ea03ac676128088965354d00db
Fuxuanmylove <2793275118@qq.com>
2025-11-28 15:29:29 +0800

    add 'len'

--- pythonic.hpp
+++ pythonic.hpp
@@ -1,1 +1,19 @@
-"" 
+#pragma once
+#include <concepts>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <vector>
+
+template <typename T>
+concept LenAble = requires(const T &c) {
+    { c.size() } -> std::convertible_to<std::size_t>;
+};
+
+template <LenAble Container> constexpr int len(const Container &c) noexcept { return static_cast<int>(c.size()); }
+
+int len(...) = delete;
+int len(const char *) = delete;
+int len(char *) = delete;
+int len(std::string_view) = delete;
+inline int len(bool) = delete;
